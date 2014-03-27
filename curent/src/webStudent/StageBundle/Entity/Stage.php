27453259c0103f7820fac12a2e55c1b061485618
Drkuhybride <sidney.falhun@live.fr>
2014-03-27 10:14:03 +0100

    ajout entity stage

---
+++ curent/src/webStudent/StageBundle/Entity/Stage.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace webStudent\StageBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Stage
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="webStudent\StageBundle\Entity\StageRepository")
+ */
+class Stage
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="note", type="integer")
+     */
+    private $note;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mission", type="string", length=255)
+     */
+    private $mission;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="date_debut", type="string", length=40)
+     */
+    private $dateDebut;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="date_fin", type="string", length=40)
+     */
+    private $dateFin;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set note
+     *
+     * @param integer $note
+     * @return Stage
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+    
+        return $this;
+    }
+
+    /**
+     * Get note
+     *
+     * @return integer 
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+
+    /**
+     * Set mission
+     *
+     * @param string $mission
+     * @return Stage
+     */
+    public function setMission($mission)
+    {
+        $this->mission = $mission;
+    
+        return $this;
+    }
+
+    /**
+     * Get mission
+     *
+     * @return string 
+     */
+    public function getMission()
+    {
+        return $this->mission;
+    }
+
+    /**
+     * Set dateDebut
+     *
+     * @param string $dateDebut
+     * @return Stage
+     */
+    public function setDateDebut($dateDebut)
+    {
+        $this->dateDebut = $dateDebut;
+    
+        return $this;
+    }
+
+    /**
+     * Get dateDebut
+     *
+     * @return string 
+     */
+    public function getDateDebut()
+    {
+        return $this->dateDebut;
+    }
+
+    /**
+     * Set dateFin
+     *
+     * @param string $dateFin
+     * @return Stage
+     */
+    public function setDateFin($dateFin)
+    {
+        $this->dateFin = $dateFin;
+    
+        return $this;
+    }
+
+    /**
+     * Get dateFin
+     *
+     * @return string 
+     */
+    public function getDateFin()
+    {
+        return $this->dateFin;
+    }
+}
