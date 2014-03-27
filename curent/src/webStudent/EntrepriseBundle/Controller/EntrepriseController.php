691b61395537661a88f1c89610eccba7605cfc43
blag <blag@rakshata.com>
2014-03-27 22:20:40 +0100

    ajout fontion showAllEntreprise (debut)

--- curent/src/webStudent/EntrepriseBundle/Controller/EntrepriseController.php
+++ curent/src/webStudent/EntrepriseBundle/Controller/EntrepriseController.php
@@ -4,6 +4,7 @@ namespace webStudent\EntrepriseBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use webStudent\EntrepriseBundle\Entity\Section;
 
 class EntrepriseController extends Controller
 {
@@ -12,10 +13,23 @@ class EntrepriseController extends Controller
         //return $this->render('webStudentEntrepriseBundle:Default:index.html.twig', array('name' => $name));
 		return $this->render('webStudentEntrepriseBundle:Entreprise:index.html.twig');
     }
+
     public function consulterEntrepriseAction()
     {
-        //return $this->render('webStudentEntrepriseBundle:Default:index.html.twig', array('name' => $name));
-		return $this->render('webStudentEntrepriseBundle:Entreprise:index.html.twig');
+        $repository = $this->getDoctrine()
+                       ->getManager()
+                       ->getRepository('webStudentEntrepriseBundle:Entreprise');
+
+        $listeEntreprises = $repository->findAll();
+
+        if ($listeEntreprises === null) {
+           throw $this->createNotFoundException('Entreprise inexistant.');
+           
+        }
+         
+        return $this->render('webStudentEntrepriseBundle:Entreprise:ConsulterEntreprise.html.twig', array(
+             'listeEntreprises' => $listeEntreprises
+            ));
     }
 
 }
